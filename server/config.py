77df51aed72111f67e0b042eb06b44f8f3661d88
lewys-miugo <lewiswambugu01@gmail.com>
2025-09-25 15:59:36 +0300

    Changes to prep repo for deployment

--- server/config.py
+++ server/config.py
@@ -1,14 +1,23 @@
 import os
+from dotenv import load_dotenv
 
+# Load variables from .env if present
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, ".env"))
+
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "super-secret-key")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", f"sqlite:///{os.path.join(basedir, 'app.db')}")
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        f"sqlite:///{os.path.join(basedir, 'app.db')}"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class DevelopmentConfig(Config):
     DEBUG = True
+
 
 class ProductionConfig(Config):
     DEBUG = False
