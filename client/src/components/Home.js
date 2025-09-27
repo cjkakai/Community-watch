a560a1b230007021b4d182b6997f0ed4499c5b22
lewys-miugo <lewiswambugu01@gmail.com>
2025-09-27 13:10:43 +0300

    fixing eslint error

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -26,8 +26,8 @@ function Home(){
       .then(([reports,officers,assignments]) => {
         // manipulating the data
         const openReports = reports.filter(r => r.status === 'open').length;
-        const pendingReports = reports.filter(r => r.status == 'pending').length;
-        const closedReports = reports.filter(r => r.status == 'closed').length;
+        const pendingReports = reports.filter(r => r.status === 'pending').length;
+        const closedReports = reports.filter(r => r.status === 'closed').length;
 
         setStats({
           totalReports:reports.length,
